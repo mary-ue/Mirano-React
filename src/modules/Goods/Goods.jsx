af91153aeb76e481a846240e459ebd7601b3df01
mary-ue <maryandreeva100@gmail.com>
2024-07-06 01:28:07 +0300

    start layout

---
+++ src/modules/Goods/Goods.jsx
@@ -0,0 +1,25 @@
+import { Cart } from '../Cart/Cart';
+import { goodsArray } from '../../goodsArray';
+import { Card } from '../Card/Card';
+
+export const Goods = () => {
+  return (
+    <section className="goods">
+      <div className="container goods__container">
+        <div className="goods__box">
+          <h2 className="goods__title">Цветы</h2>
+
+          <ul className="goods__list">
+            {goodsArray.map((item) => (
+              <li className="goods__item" key={item.id}>
+                <Card className="goods__card" {...item} />
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <Cart />
+      </div>
+    </section>
+  );
+};
