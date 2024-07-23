3407a0005f43ebc54b91ce5a76ceb21c17ec1e98
mary-ue <maryandreeva100@gmail.com>
2024-07-24 00:19:49 +0300

    choices position

--- src/modules/Choices/Choices.jsx
+++ src/modules/Choices/Choices.jsx
@@ -1,5 +1,7 @@
 import cn from 'classnames';
 import s from './Choices.module.scss';
+import { useEffect, useRef } from 'react';
+import { adjustElementPosition, debounce } from '../../utils';
 
 export const Choices = ({
   children,
@@ -8,13 +10,37 @@ export const Choices = ({
   isOpen,
   onToggle,
 }) => {
+  const choiceRef = useRef(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      adjustElementPosition(choiceRef.current);
+    }
+
+    const debouncedAdjustElementPosition = debounce(() => {
+      if (isOpen) {
+        adjustElementPosition(choiceRef.current);
+      }
+    }, 100);
+
+    window.addEventListener('resize', debouncedAdjustElementPosition);
+
+    return () => {
+      window.removeEventListener('resize', debouncedAdjustElementPosition);
+    };
+  }, [isOpen]);
+
   return (
     <div className={cn(s.choices, className)}>
       <button className={s.btn} type="button" onClick={onToggle}>
         {buttonLabel}
       </button>
 
-      {isOpen && <div className={s.box}>{children}</div>}
+      {isOpen && (
+        <div className={s.box} ref={choiceRef}>
+          {children}
+        </div>
+      )}
     </div>
   );
 };
--- src/utils.js
+++ src/utils.js
@@ -10,7 +10,7 @@ export const getValidFilters = (filters) => {
   return validFilters;
 };
 
-export const debounce = (fn, msec) => {
+export const debounce = (fn, msec = 300) => {
   let lastCall = 0;
   let lastCallTimer = 0;
 
@@ -29,3 +29,34 @@ export const debounce = (fn, msec) => {
 };
 
 export const isNumber = (n) => !isNaN(parseInt(n) && isFinite(n));
+
+export const adjustElementPosition = (element, count = 0) => {
+  const rect = element.getBoundingClientRect();
+  const viewportWidth = window.innerWidth;
+
+  if (rect.left < 0) {
+    element.style.cssText = `
+      left: 0;
+      right: auto;
+      transform: translateX(0)
+    `;
+  } else if (rect.right > viewportWidth) {
+    element.style.cssText = `
+      left: auto;
+      right: 0;
+      transform: translateX(0)
+    `;
+  } else {
+    element.style.cssText = `
+      left: 50%;
+      right: auto;
+      transform: translateX(-50%)
+    `;
+  }
+
+  const postRect = element.getBoundingClientRect();
+
+  if ((postRect.left < 0 || postRect.right > viewportWidth) && count > 3) {
+    adjustElementPosition(element, ++count);
+  }
+};
